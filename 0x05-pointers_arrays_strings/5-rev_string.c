b5bb27e7de47b0688c2d4d2d83973a0c8ed0ba4b
Chammma37 <s37879189@gmail.com>
2023-05-03 15:56:36 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,14 +6,13 @@
  * Return : void
  */
 
-void rev_string(char *s);
+void rev_string(char *s)
 {
 	int len = 0, index = 0;
 	char tmp;
-	
+
 	while (s[index++])
 	len++;
-	
 	for (index = len - 1; index >= len / 2; index--)
 	{
 	tmp = s[index];
